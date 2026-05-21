8c41ccefa0951688b17a89bb2cd3d48d8a740e46
Chloé Vaillancourt <chloe.vaillancourt.2@ulaval.ca>
2026-05-21 09:01:47 -0400

    correction pour le fichier d'expression

--- prep_tensorQTL.R
+++ prep_tensorQTL.R
@@ -10,9 +10,13 @@ source("liftOver.R")
 # =============================
 
 # Fusion avec l'expression
-expr_data <- read.delim("data/Adjusted_expression_values.txt", sep = "\t", check.names = FALSE)
+expr_data <- read.delim("/home/chloev/links/projects/def-bureau/expression_genes/Adjusted_expression_values.txt", sep = "\t", check.names = FALSE)
 # Harmonisation du nom de la première colonnne
 colnames(expr_data)[1] <- "probe_id"
+
+sondes_expr <- read.table("/home/chloev/links/projects/def-bureau/expression_genes/liste_sondes_expr_GC.txt", header = FALSE, stringsAsFactors = FALSE)$V1
+expr_data <- expr_data[expr_data$probe_id %in% sondes_expr, ]
+
 # Fusion
 expr_hg38 <- merge(lifted, expr_data, by = "probe_id")
 
@@ -21,7 +25,7 @@ expr_hg38 <- merge(lifted, expr_data, by = "probe_id")
 # ===========================
 
 # Chargement des données du phénotype mis à jour
-pheno <- read.table("data/GCbroad_plink.txt", header = FALSE, stringsAsFactors = FALSE)
+pheno <- read.table("/lustre09/project/6033529/schizo/data/WGS_bs_2022/500_samples_cag/RetroFunRVS/objets_ped/GCbroad_plink.txt", header = FALSE, stringsAsFactors = FALSE)
 colnames(pheno) <- c("FID", "IID", "Diagnostic")
 pheno$IID <- as.character(pheno$IID)
 rownames(pheno) <- pheno$IID
@@ -36,6 +40,11 @@ cols_individu_expr <- setdiff(colnames(expr_hg38), c("probe_id", "illumina_id", 
 
 # L'individu doit exister dans tous les fichiers
 ind_existants <- intersect(cols_individu_expr, pheno$IID)
+
+plink_fam <- read.table("/lustre09/project/6033529/schizo/data/WGS_bs_2022/500_samples_cag/genotyping/omni_grch38.fam", header = FALSE, stringsAsFactors = FALSE)
+plink_ids <- as.character(plink_fam$V2)
+
+ind_existants <- intersect(intersect(cols_individu_expr, pheno$IID), "\n")
 
 # =========================================
 # Formatage de la matrice d'expression BED
@@ -75,12 +84,15 @@ pheno_tensorqtl <- cbind(`#id` = rownames(pheno_t), pheno_t)
 # ===========
 
 # Sauvegarde du fichier de phénotypes
-write.table(pheno_tensorqtl, "results/eQTL/phenotypes.txt", sep = "\t", 
+write.table(pheno_tensorqtl, "/home/chloev/links/projects/def-bureau/expression_genes/resultats/phenotypes.txt", sep = "\t", 
                         row.names = FALSE, col.names = TRUE, quote = FALSE)
 
 # Sauvegarde du fichier BED brut
-write.table(tensorqtl_bed, "results/eQTL/expression_hg38.bed", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+write.table(tensorqtl_bed, "/home/chloev/links/projects/def-bureau/expression_genes/resultats/expression_hg38.bed", sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
 
 # Sauvegarde en .bed.gz 
-tensorqtl_bed_zip <- bgzip("results/eQTL/expression_hg38.bed", dest= "results/eQTL/expression_hg38.bed.gz", overwrite = TRUE)
-indexTabix(tensorqtl_bed_zip, format = "bed")
+#tensorqtl_bed_zip <- bgzip("results/eQTL/expression_hg38.bed", dest= "results/eQTL/expression_hg38.bed.gz", overwrite = TRUE)
+#indexTabix(tensorqtl_bed_zip, format = "bed")
+
+out <- bgzip("/home/chloev/links/projects/def-bureau/expression_genes/resultats/expression_hg38.bed", dest = "/home/chloev/links/projects/def-bureau/expression_genes/resultats/expression_hg38.bed.gz", overwrite = TRUE)
+indexTabix(out, format = "bed")
