a949e3d8a5addbe22b483d8545b67d11de697583
Chloé Vaillancourt <chloe.vaillancourt.2@ulaval.ca>
2026-05-27 15:09:23 -0400

    Correction pour le fichier

--- Sondes_exprimees.R
+++ Sondes_exprimees.R
@@ -1,20 +1,8 @@
 library(data.table)
+library(readxl)
 
-# =======================
-# Chargement des données
-# =======================
+filtre <- read_xlsx("data/table_expressed_probes_sans_lame_corr_bg_RMA_REML_GCbr_543sujets.xlsx")
+sondes_valides <- unique(na.omit(as.character(filtre[[1]])))
 
-mat <- fread("data/sondes_exprimees_fam_p05_75percent_549sujets.csv", sep = ";", header = TRUE, data.table = FALSE)
-rownames(mat) <- sub("^X", "", mat[[1]])
-mat <- mat[, -1]
-mat_bool <- as.matrix(mat) == "TRUE" 
-
-# Sondes exprimées doivent faire partie d'une famille ayant au mons 4 membres
-sonde_ok <- rowsums(mat_bool, na.rm = TRUE) >= 1
-sondes_filtrees <- rownames(mat_bool)[sondes_ok]
-
-expression <- fread("data/Adjusted_expression_values.txt", select = 1)
-colnames(expression)[1] <- "probe_id"
-sondes_finales(expression$probe_id, sondes_filtrees)
-
-fwrite(data.frame(probe_id = sondes_finales), "results/liste_sondes_exprimees.txt", col.names = FALSE)
+# sauvegarde
+write.table(data.frame(probe_id = sondes_valides), "results/liste_sondes_exprimees.txt", row.names = FALSE, col.names = FALSE, quote = FALSE, sep = "\t")
