c7e8aef91115069e7739177f49bf03202993b93b
Chloé Vaillancourt <chloe.vaillancourt.2@ulaval.ca>
2026-07-24 11:49:38 -0400

    Ajout de la covariable sexe et dichotomisation du score

--- Association_phenotypique.R
+++ Association_phenotypique.R
@@ -2,7 +2,6 @@
 # Association_phenotypique_p90.R
 # Adaptation de la méthodologie de Mazzarotto et al. 2025 à la cohorte Chagnon
 # Analyse du fardeau de variants rares IOGC dichotomisé au 90e percentile (P90 vs reste)
-# Pas de PRS ni de prévalence populationnelle (K) : métriques basées sur l'échelle observée.
 #
 # Entrées :
 #  - results/scores_iogc/score_iogc_*_atteints*.txt
