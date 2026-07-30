084f4c846e7c19608deee3222b6c1525676ef12b
Chloé Vaillancourt <chloe.vaillancourt.2@ulaval.ca>
2026-07-30 14:50:13 -0400

    Modification des chemins des fichiers pour expression_genes

--- Retrait_effet_eQTL.R
+++ Retrait_effet_eQTL.R
@@ -17,8 +17,8 @@ library(data.table)
 # -----------------------
 
 eqtl_results <- fread("/lustre09/project/6000443/expression_genes/resultats/tensorqtl/tensorqtl_out.cis_qtl.txt") 
-expression_mat <- fread("/lustre09/project/6000443/chloev/Adjusted_expression_values.txt")
-raw_geno <- fread("/home/chloev/links/projects/def-bureau/chloev/retrait_effet_eqtl/genotypes_eqtl.raw")
+expression_mat <- fread("/lustre09/project/6000443/expression_genes/Adjusted_expression_values.txt")
+raw_geno <- fread("/lustre09/project/6000443/expression_genes/resultats/retrait_effet_eqtl/genotypes_eqtl.raw")
 
 setnames(expression_mat, old = 1, new = "probe")
 
@@ -26,44 +26,28 @@ setnames(expression_mat, old = 1, new = "probe")
 # Préparation des matrices
 # -------------------------
 
-# --Sélectionner le lead cis-eQTL de chaque sonde
-# lead cis-eQTL = variant avec la plus petite p-value
+# -- électionner le lead cis-eQTL de chaque sonde (variant avec la plus petite p-value)
 setorder(eqtl_results, phenotype_id, pval_nominal)
 lead_eqtls <- eqtl_results[!duplicated(phenotype_id)]
 
-mat_samples <- intersect(colnames(expression_mat)[-1], sub("^[^_]+_", "", as.character(raw_geno$IID)))
-expression_clean <- copy(expression_mat) # --conserver toutes les sondes; seules celles avec un lead eQTL seront ajustées
+# --Identification
+raw_geno_samples_clean <- sub("^[^_]+_", "", as.character(raw_geno$IID))
+mat_samples <- intersect(colnames(expression_mat)[-1], raw_geno_samples_clean)
+
+expression_clean <- copy(expression_mat)
 
 # --Restreindre la table des lead eQTL aux sondes réellement présentes dans la matrice
 lead_eqtls_clean <- lead_eqtls[phenotype_id %in% expression_clean$probe]
 setorder(lead_eqtls_clean, phenotype_id)
 setorder(expression_clean, probe)
 
-
-# --Construire la matrice de génotypes transposée
-raw_geno[, IID := sub("^[^_]+_", "", as.character(IID))]
-colnames(raw_geno) <- gsub("_[ACGT]+$", "", colnames(raw_geno))
-
-# --Fonction pour normaliser un variant_id en clé insensible à l'ordre REF/ALT
-make_key <- function(id) {
-  parts <- tstrsplit(id, ":", fixed = TRUE)
-  chr <- parts[[1]]; pos <- parts[[2]]
-  a1 <- parts[[3]]; a2 <- parts[[4]]
-  paste(chr, pos, pmin(a1, a2), pmax(a1, a2), sep = ":")
-}
-
-# --Construire les clés des deux côtés
-lead_eqtls_clean[, match_key := make_key(variant_id)]
-
-raw_ids <- colnames(raw_geno)[colnames(raw_geno) %like% "^chr"]  # exclure FID/IID/PAT/etc.
-key_to_rawid <- setNames(raw_ids, make_key(raw_ids))
-
-# --Vérifier le gain avant de continuer
-sum(lead_eqtls_clean$match_key %in% names(key_to_rawid))
-
-raw_geno_filtered <- raw_geno[IID %in% mat_samples]
-cols_to_keep <- c("IID", intersect(colnames(raw_geno), lead_eqtls_clean$variant_id))
+# --Matrice du génotype
+colnames(raw_geno) <- gsub("_[A-Za-z0-9]+$", "", colnames(raw_geno))
+raw_geno_filtered <- raw_geno[sub("^[^_]+_", "", as.character(IID)) %in% mat_samples]
+cols_to_keep <- c("IID", intersect(colnames(raw_geno_filtered), lead_eqtls_clean$variant_id))
 geno_clean <- raw_geno_filtered[, ..cols_to_keep]
+
+geno_clean[, IID := sub("^[^_]+_", "", as.character(IID))]
 geno_t <- transpose(geno_clean, make.names = "IID", keep.names = "variant_id")
 
 # ----------------------------------------------------
